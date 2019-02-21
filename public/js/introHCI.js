de830661c5b45a7ff4514a87137eb5ff5661c0b5
Angelica Ramos <amr052@ucsd.edu>
2019-02-21 13:25:50 -0800

    this was hard

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -3,11 +3,21 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+
 });
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$("button").click(projectClick);
+}
+
+function projectClick() { 
+    // prevent the page from reloading      
+    
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).ga("send", "event", 'like', 'click');
+    
 }
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var projects = require('../projects.json');
 /*
  * GET home page.
  */
-
 exports.view = function(request, response){
+	projects['viewAlt']=false;
   	response.render('index', projects);
 };
+exports.viewAlt = function(request, response){
+	projects['viewAlt']=true;
+  	response.render('index', projects);
+};
+// exports.view = function(req, res){
+// 	data['viewAlt']=false;
+//   	response.render('index', data);
+// };
+
+// exports.viewAlt = function(rew,res){
+// 	data['viewAlt']=true;
+// 	res.render('index',data);
+// }
+
